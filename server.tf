ada0d8f914d78d10d4f90f90ae5fdc3e134d3c6c
CodeBaazigar <rajjoshi.pw@gmail.com>
2025-09-12 20:55:56 +0530

    This is my Terraform Script for testing and Prod server

--- server.tf
+++ server.tf
@@ -18,8 +18,14 @@ resource "aws_instance" "Prod_server" {
   ami           = "ami-07e075f00c26b085a"
   instance_type = "t3.micro"
   key_name      = "key123"
-  vpc_security_group_ids = ["sg-0c64674a9902a10b3"]                                                                                                                                                                                                                                                                       root_block_device {                                                                                                                                           volume_size = 8   # storage in GB
+  vpc_security_group_ids = ["sg-0c64674a9902a10b3"]
+
+  root_block_device {
+    volume_size = 8   # storage in GB
     volume_type = "gp3"
-  }                                                                                                                                                                                                                                                                                                                       tags = {
+  }
+
+  tags = {
     Name = "Prod_server"
-  }                                                                                                                                                         }
+  }
+}
